6de51d04621fd26ce96370c0dd43e3a922edfe61
shizhao <shizhao@gmail.com>
2017-04-05 20:26:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh" dir="ltr">
+<head>
+<meta charset="UTF-8"/>
+<meta name="robots" content="index, follow">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>keywords list</title>
+
+<body>
+</body>
+</html>
